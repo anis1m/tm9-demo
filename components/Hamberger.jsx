8194060f9ff8fdd04088ae60c22c2180544d42a0
Anis Mahat <amahat@hexalytics.com>
2026-02-10 08:52:01 +0530

    changes

--- components/Hamberger.jsx
+++ components/Hamberger.jsx
@@ -2,20 +2,20 @@ import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Hamberger({title}){
-    return (
-        <View style={styles.header}>
-            <Ionicons
-              name="arrow-back"
-              size={22}
-              color="#fff"
-              onPress={() => {
-                router.back();
-              }}
-            />
-            <Text style={styles.headerTitle}>{title}</Text>
-          </View>
-    )
+export default function Hamberger({ title }) {
+  return (
+    <View style={styles.header}>
+      <Ionicons
+        name="arrow-back"
+        size={22}
+        color="#fff"
+        onPress={() => {
+          router.back();
+        }}
+      />
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -30,4 +30,4 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     marginLeft: 12,
   },
-})
+});
